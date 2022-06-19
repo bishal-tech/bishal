265d6feb01cf467b3f86dc45f2da35e7fe516fc4
Bishal Debsharma <bishal.tech.com@gmail.com>
2022-06-19 21:54:26 +0530

    add pt slider

---
+++ assets/js/total-script.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+  $(".brands-slider").slick({
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    arrows: false,
+    dots: false,
+    speed: 300,
+    slidesToScroll: 1,
+    pauseOnHover: true,
+    centerMode: false,
+    centerPadding: "0px",
+    slidesToShow: 8,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: false,
+          centerPadding: "0px",
+          slidesToShow: 6,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: false,
+          centerPadding: "0px",
+          slidesToShow: 2,
+          slidesToShow: 4,
+        },
+      },
+    ],
+  });
+});
